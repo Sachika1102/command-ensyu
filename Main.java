999caef1c14b998591c897f990f03e0bd85872f8
sachika <s.tsukeyanagi.sys23@morijyobi.ac.jp>
2023-11-10 11:34:10 +0900

    Main.javaに1回の実行で２人分計算できるように変更した

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.math.RoundingMode;
 
 public class main{
 	public static void main(String[] args){
+		System.out.println("1人目のBMIを表示");
 		double height = 150.0;
 		double weight = 42.0;
 		System.out.println("身長:" + height + "(cm)");
@@ -13,5 +14,17 @@ public class main{
 		BigDecimal bmi = new BigDecimal(result);
 		bmi = bmi.setScale(2,RoundingMode.HALF_UP);
 		System.out.println("BMIは" + bmi + "です");
+
+		System.out.println("2人目のBMIを表示");
+		double height2 = 175.0;
+		double weight2 = 65.0;
+		System.out.println("身長:" + height2 + "(cm)");
+		System.out.println("体重:" + weight2 + "(kg)");
+
+		double result2 = weight / (height*height) * 10000;
+
+		BigDecimal bmi2 = new BigDecimal(result2);
+		bmi2 = bmi2.setScale(2,RoundingMode.HALF_UP);
+		System.out.println("BMIは" + bmi2 + "です");
 	}
 }
